9686c0f5da7ff185e7b7d2f34a8fd14d56ac534c
Sunil Gopinath <sxg282@gmail.com>
2012-03-21 23:05:53 -0400

    playing around with NSLog

--- main.m
+++ main.m
@@ -21,6 +21,40 @@ int main(int argc, char *argv[]) {
 	UIDevice *device = [UIDevice currentDevice];
 	NSString *version = device.systemVersion;
 	NSLog(@"Version of iOS: %@", version);	//Output an NString with %@.
+	
+	CGFloat num = 3.14159;
+	CGFloat *pf = &num;
+	NSLog(@"value of a pointer: %p.", pf); //gets the pointer value, if g
+										  //would get num value
+	
+	CGPoint cgPoint = CGPointMake(0.0, 0.0);
+	//we can use UIKit functions to print out points
+	NSLog(@"%@", NSStringFromCGPoint(cgPoint));
+	
+	//Pass five arguments to NSLog.
+	CGRect r = CGRectMake(0.0, 0.0, 320.0, 480.0);
+	NSLog(@"r == (%g, %g), %g × %g",
+		  r.origin.x,
+		  r.origin.y,
+		  r.size.width,
+		  r.size.height
+		  );
+	//or use the UIKit function
+	NSLog(@"rect1: %@", NSStringFromCGRect(r));
+	
+	//Let s be a pointer to the main screen of the app.
+	UIScreen *s = [UIScreen mainScreen];
+	//Get the location and size of the screen.
+	CGRect b = [s bounds];
+	NSLog(@"screen dimension: %@", NSStringFromCGRect(b));
+	
+	//since bounds is also a property
+	CGRect propertyBounds = s.bounds;
+	NSLog(@"property version: %@", NSStringFromCGRect(propertyBounds));
+	
+	//nest brackets
+	CGRect telescoped = [[UIScreen mainScreen] bounds];
+	NSLog(@"telescoped:%@", NSStringFromCGRect(telescoped));
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
     return retVal;
